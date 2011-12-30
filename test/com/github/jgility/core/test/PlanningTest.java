4976224b5d2f143f80d65906c9f32191b580d927
Karsten Schulz <lennylinux.ks@googlemail.com>
2011-12-30 11:00:02 +0100

    Test: add releaseplanningutils in junit-test-cases
    Change-Id: I77879bfd22ef232df1780af0e97fd070cc33cb98

--- test/com/github/jgility/core/test/PlanningTest.java
+++ test/com/github/jgility/core/test/PlanningTest.java
@@ -7,11 +7,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.jgility.core.planning.IRelease;
 import com.github.jgility.core.planning.Iteration;
 import com.github.jgility.core.planning.Release;
 import com.github.jgility.core.project.Person;
 import com.github.jgility.core.project.Product;
 import com.github.jgility.core.project.Project;
+import com.github.jgility.core.util.ReleasePlanningUtils;
 
 public class PlanningTest
 {
@@ -33,10 +35,10 @@ public class PlanningTest
     @Test
     public void testIterationReleasePlaning()
     {
-        Calendar start = new GregorianCalendar( 2012, 3, 1 );
-        Calendar end1 = new GregorianCalendar( 2012, 3, 15 );
-        Calendar start1 = new GregorianCalendar( 2012, 3, 15 );
-        Calendar end = new GregorianCalendar( 2012, 4, 1 );
+        Calendar start = new GregorianCalendar( 2012, 2, 1 );
+        Calendar end1 = new GregorianCalendar( 2012, 2, 15 );
+        Calendar start1 = new GregorianCalendar( 2012, 2, 15 );
+        Calendar end = new GregorianCalendar( 2012, 2, 28 );
 
         Release release = new Release( start, end );
 
@@ -50,10 +52,45 @@ public class PlanningTest
         project.addReleasePlan( release );
     }
 
-    @Test
-    public void testDateChangeAlgorithm()
+    private void checkDate( Calendar first, Calendar second )
     {
-        testIterationReleasePlaning();
+        Assert.assertTrue( first.get( Calendar.YEAR ) == second.get( Calendar.YEAR ) );
+        Assert.assertTrue( first.get( Calendar.MONTH ) == second.get( Calendar.MONTH ) );
+        Assert.assertTrue( first.get( Calendar.DAY_OF_MONTH ) == second.get( Calendar.DAY_OF_MONTH ) );
     }
 
+    @Test
+    public void testDateChangeAlgorithmCut()
+    {
+        testIterationReleasePlaning();
+        IRelease release = project.getReleasePlan().get( 0 );
+        ReleasePlanningUtils.changePlanCutSubPlan( release, new GregorianCalendar( 2012, 2, 12 ),
+                                                  new GregorianCalendar( 2012, 3, 1 ) );
+        checkDate( release.getIteration( 0 ).getStart(), new GregorianCalendar( 2012, 2, 12 ) );
+        checkDate( release.getIteration( 1 ).getEnd(), new GregorianCalendar( 2012, 3, 1 ) );
+
+        ReleasePlanningUtils.changePlanCutSubPlan( release, new GregorianCalendar( 2012, 2, 16 ),
+                                                  new GregorianCalendar( 2012, 2, 30 ) );
+        Assert.assertTrue( release.size() == 1 );
+
+        try
+        {
+            ReleasePlanningUtils.changePlanCutSubPlan( release,
+                                                      new GregorianCalendar( 2012, 4, 16 ),
+                                                      new GregorianCalendar( 2012, 4, 12 ) );
+            Assert.fail();
+        }
+        catch ( IllegalArgumentException e )
+        {
+        }
+    }
+
+    @Test
+    public void testDateChangeAlgorithmPercent()
+    {
+        testIterationReleasePlaning();
+        IRelease release = project.getReleasePlan().get( 0 );
+        ReleasePlanningUtils.changePlanPerPercent( release, new GregorianCalendar( 2012, 3, 1 ),
+                                                  new GregorianCalendar( 2012, 3, 19 ) );
+    }
 }
