0107ada39305ee2144aedd2ca47ea0ab9e4eb46a
Karsten Schulz <lennylinux.ks@googlemail.com>
2012-01-18 12:25:43 +0100

    Test: fixing junit-test for jaxb (jpa for xml)

--- test/com/github/jgility/core/test/JAXBTest.java
+++ test/com/github/jgility/core/test/JAXBTest.java
@@ -1,7 +1,6 @@
 package com.github.jgility.core.test;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
@@ -19,6 +18,7 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.github.jgility.core.planning.Backlog;
 import com.github.jgility.core.planning.IBacklog;
 import com.github.jgility.core.planning.Iteration;
 import com.github.jgility.core.planning.Release;
@@ -27,8 +27,10 @@ import com.github.jgility.core.project.IProduct;
 import com.github.jgility.core.project.Person;
 import com.github.jgility.core.project.Product;
 import com.github.jgility.core.project.Project;
+import com.github.jgility.core.project.Team;
 import com.github.jgility.core.requirement.IIterationRequirement;
 import com.github.jgility.core.requirement.IProductRequirement;
+import com.github.jgility.core.requirement.ImplementableStory;
 import com.github.jgility.core.requirement.IterationStory;
 import com.github.jgility.core.requirement.Priority;
 import com.github.jgility.core.requirement.ProductStory;
@@ -73,7 +75,10 @@ public class JAXBTest
         JAXBContext context;
         try (final FileWriter w = new FileWriter( "database.xml" ))
         {
-            context = JAXBContext.newInstance( Product.class );
+            context =
+                JAXBContext.newInstance( Product.class, Project.class, Backlog.class,
+                                         ProductStory.class, ImplementableStory.class, Team.class,
+                                         Person.class, Release.class, Iteration.class );
             Marshaller m = context.createMarshaller();
             m.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
             m.marshal( product, w );
@@ -87,17 +92,21 @@ public class JAXBTest
     @Test
     public void testRead()
     {
-        try (final FileReader f = new FileReader( "database.xml" ))
+        try
         {
-            JAXBContext context = JAXBContext.newInstance( Product.class );
+            JAXBContext context =
+                JAXBContext.newInstance( Product.class, Project.class, Backlog.class,
+                                         ProductStory.class, ImplementableStory.class, Team.class,
+                                         Person.class, Release.class, Iteration.class );
             Unmarshaller um = context.createUnmarshaller();
             JAXBElement<Product> element =
                 um.unmarshal( new StreamSource( new File( "database.xml" ) ), Product.class );
 
             product = element.getValue();
         }
-        catch ( final JAXBException | IOException e )
+        catch ( JAXBException e )
         {
+            System.out.println( e );
             Assert.fail( e.getLocalizedMessage() );
         }
 
